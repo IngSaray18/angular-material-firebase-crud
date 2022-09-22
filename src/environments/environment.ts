170f1ba453858dd3adb9a260a692a4e5872f8c8b
IngSaray18 <pedrosaray18@hotmail.com>
2022-09-21 23:17:22 -0500

    fe

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAi4fsTjWt2K8Fk31dDLK33sFlaZO9SjBw",
+    authDomain: "library-aeaf0.firebaseapp.com",
+    databaseURL: "https://library-aeaf0-default-rtdb.firebaseio.com",
+    projectId: "library-aeaf0",
+    storageBucket: "library-aeaf0.appspot.com",
+    messagingSenderId: "1079207812695",
+    appId: "1:1079207812695:web:530d530a097fb2f8800d12",
+    measurementId: "G-SKXL9J2ZB3"
+  }
+
 };
 
 /*
